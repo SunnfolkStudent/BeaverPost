57fb23ee9486f15077f2836c4e59483f4f0d11d8
WiDuck1 <87336958+WiDuck1@users.noreply.github.com>
2024-09-19 21:07:08 +0200

    CountdownTimer
    CountdownTimer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+
+
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Text textClock;
+    
+    private float countdownTimerDuration;
+    private float countdownTimerStartTime;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textClock = GetComponent<Text>();
+        CountdownTimerReset(30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // default = timer finished
+        string timerMessage = "Countdown has finished";
+        int timeLeft = (int)CountdownTimerSecondsRemaining();
+
+            if (timeLeft > 0)
+            {
+                timerMessage = "Time remaining: " + LeadingZero( timeLeft );
+            
+                textClock.text = timerMessage;
+            }
+            
+            else
+            {
+                print("Countdown has finished");
+            }
+
+        
+    }
+    
+    private void CountdownTimerReset(float delayInSeconds)
+    {
+        countdownTimerDuration = delayInSeconds;
+        countdownTimerStartTime = Time.time;
+    }
+
+    private float CountdownTimerSecondsRemaining()
+    {
+        float elapsedSeconds = Time.time - countdownTimerStartTime;
+        float timeLeft = countdownTimerDuration - elapsedSeconds;
+        return timeLeft;
+    }
+
+    private string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
