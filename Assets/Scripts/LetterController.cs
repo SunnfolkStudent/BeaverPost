5d4f3ea4ea74272680243596516ffbbdec78b0da
WiDuck1 <87336958+WiDuck1@users.noreply.github.com>
2024-09-20 11:28:13 +0200

    Merged main with Maya's work

---
+++ Assets/Scripts/LetterController.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LetterController : MonoBehaviour
+{
+    public Transform player;
+    public bool canPickup = false;
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            transform.SetParent(player);
+            transform.position = new Vector2(player.position.x, player.position.y + 1);
+        }
+    }*/
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canPickup = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (canPickup == true && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            transform.SetParent(player);
+            transform.position = new Vector2(player.position.x, player.position.y + 1);
+            print("hi");
+        }
+    }
+}
